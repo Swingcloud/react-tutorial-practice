29e578121573fdba3fabb99ba37b7d36f27c0802
Swingcloud <sam@swingcloud.app>
2018-06-29 17:07:29 +0800

    refactor to es 6 part two

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,13 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var queryString = require('query-string');
-var api = require('../utils/api');
-var Link = require('react-router-dom').Link;
-var PlayerPreview = require('./PlayerPreview');
-var Loading = require('./Loading');
+const React = require('react');
+const PropTypes = require('prop-types');
+const queryString = require('query-string');
+const api = require('../utils/api');
+const Link = require('react-router-dom').Link;
+const PlayerPreview = require('./PlayerPreview');
+const Loading = require('./Loading');
 
-function Profile(props) {
-  var info = props.info;
+function Profile({ info }) {
+
   return (
     <PlayerPreview
       avatar={info.avatar_url}
@@ -30,14 +30,14 @@ Profile.propTypes = {
   info: PropTypes.object.isRequired
 }
 
-function Player(props) {
+function Player({ label, score, profile }) {
   return (
     <div>
-      <h1 className="header">{props.label}</h1>
-      <h3 style={{textAlign: 'center'}}>
-        Score: {props.score}
+      <h1 className="header">{label}</h1>
+      <h3 style={{ textAlign: 'center' }}>
+        Score: {score}
       </h3>
-      <Profile info={props.profile} />
+      <Profile info={profile} />
     </div>
   )
 }
@@ -48,7 +48,7 @@ Player.propTypes = {
   profile: PropTypes.object.isRequired
 }
 
-class Results extends React.Component{
+class Results extends React.Component {
   constructor(props) {
     super(props);
 
@@ -60,35 +60,28 @@ class Results extends React.Component{
     }
   }
   componentDidMount() {
-    var players = queryString.parse(this.props.location.search)
+    var { playerOneName, playerTwoName } = queryString.parse(this.props.location.search)
     api.battle([
-      players.playerOneName,
-      players.playerTwoName
-    ]).then(function(results){
+      playerOneName,
+      playerTwoName
+    ]).then((results) => {
       if (results === null) {
-        return this.setState(function(){
-          return {
-            error: 'Look like there was an error',
-            loading: false
-          };
-        });
+        return this.setState(() => ({
+          error: 'Look like there was an error',
+          loading: false
+        }))
       }
 
-      this.setState(function(){
-        return {
-          error: null,
-          winner: results[0],
-          loser: results[1],
-          loading: false
-        }
-      })
-    }.bind(this));
+      this.setState(() => ({
+        error: null,
+        winner: results[0],
+        loser: results[1],
+        loading: false
+      }))
+    });
   }
   render() {
-    var error = this.state.error;
-    var winner = this.state.winner;
-    var loser = this.state.loser;
-    var loading = this.state.loading;
+    const {error, winner, loser, loading } = this.state
 
     if (loading === true) {
       return <Loading />
@@ -102,7 +95,7 @@ class Results extends React.Component{
         </div>
       )
     }
-    return(
+    return (
       <div className='row'>
         <Player
           label='Winner'
